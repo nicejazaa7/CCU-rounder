1e0830687c2020c4d016d19e8d735f2217347a4b
nicejazaa7 <nicejazaa7@gmail.com>
2025-08-12 22:56:42 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ccu-rounder/',  // important for GitHub Pages
+  base: '/CCU-rounder/',   // must match repo name exactly (case-sensitive)
 })
